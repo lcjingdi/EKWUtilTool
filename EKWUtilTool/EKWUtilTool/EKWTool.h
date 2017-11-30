68872f2dbde35f82a17df99044638b412e965baa
lcjingdi <lcjingdi@gmail.com>
2017-11-30 18:43:03 +0800

    2

---
+++ EKWUtilTool/EKWUtilTool/EKWTool.h
@@ -0,0 +1,15 @@
+//
+//  EKWTool.h
+//  EKWUtilTool
+//
+//  Created by lcjingdi on 2017/11/30.
+//  Copyright © 2017年 lcjingdi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EKWTool : NSObject
+
++ (void)printJD;
+
+@end
---
+++ EKWUtilTool/EKWUtilTool/EKWTool.m
@@ -0,0 +1,15 @@
+//
+//  EKWTool.m
+//  EKWUtilTool
+//
+//  Created by lcjingdi on 2017/11/30.
+//  Copyright © 2017年 lcjingdi. All rights reserved.
+//
+
+#import "EKWTool.h"
+
+@implementation EKWTool
++ (void)printJD {
+    NSLog(@"景迪");
+}
+@end
